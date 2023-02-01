6fbdeecdfe00c3605ec0b0e76e68a48d429eed8a
Luiz Henrique Neves Doretto <luiz.doretto@e-core.com>
2023-02-01 10:02:49 -0300

    create main layout to _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import MainLayout from '@/components/layout/mainLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </ChakraProvider>
   );
 }
