7ae10597ef6a45142010c9ef7f30a4ff0880b91a
Luiz Henrique Neves Doretto <luiz.doretto@e-core.com>
2023-02-02 11:56:58 -0300

    create event card and apply logic

---
+++ src/components/events/event.tsx
@@ -0,0 +1,78 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Grid,
+  Heading,
+} from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import { dateTimeToSeconds, secondsToTimes } from '@/utils/date';
+import { useInterval } from '@/hooks/use-interval';
+import { Number } from '../number';
+
+type EventProps = {
+  name: string;
+  date: Date;
+  hour?: number;
+  min?: number;
+};
+
+export const Event = ({ name = 'Event Name', date, hour, min }: EventProps) => {
+  const dateFormatted = date.toLocaleDateString('pt-br');
+  const hasHoursAndMinutes = hour && min;
+
+  const [diferenceInSeconds, setDiferenceInSeconds] = useState(
+    dateTimeToSeconds(date) - dateTimeToSeconds(new Date()),
+  );
+  const [times, setTimes] = useState<string[]>();
+
+  useInterval(() => {
+    setDiferenceInSeconds(diferenceInSeconds - 1);
+  }, 1000);
+
+  useEffect(() => {
+    setTimes(secondsToTimes(diferenceInSeconds));
+  }, [diferenceInSeconds]);
+
+  return (
+    <Card>
+      <CardHeader p={3} display="flex" justifyContent="space-between">
+        <Heading size="sm">{name}</Heading>
+        <Heading size="sm">
+          {dateFormatted} {hasHoursAndMinutes && `- ${hour}:${min}`}
+        </Heading>
+      </CardHeader>
+      <CardBody px={2} py={2}>
+        {times && (
+          <Grid templateColumns="repeat(3, minmax(0, auto))" row={2} gap={5}>
+            <Box textAlign="center">
+              <Number number={times[0]} />
+              Year(s)
+            </Box>
+            <Box textAlign="center">
+              <Number number={times[1]} />
+              Month(s)
+            </Box>
+            <Box textAlign="center">
+              <Number number={times[2]} />
+              Day(s)
+            </Box>
+            <Box textAlign="center">
+              <Number number={times[3]} />
+              Hours
+            </Box>
+            <Box textAlign="center">
+              <Number number={times[4]} />
+              Minutes
+            </Box>
+            <Box textAlign="center">
+              <Number number={times[5]} />
+              Sec
+            </Box>
+          </Grid>
+        )}
+      </CardBody>
+    </Card>
+  );
+};
